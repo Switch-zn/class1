b57dbd4978736a399898942e41b8c6b8ec61643c
Dev-zn <devzn619@gmail.com>
2024-10-16 03:41:59 -0700

    Login form

--- src/Props/Counter.jsx
+++ src/Props/Counter.jsx
@@ -5,25 +5,27 @@ const Counter = () => {
   const [Name, setName]= useState ("")
   return (
 
-  //   <div className=" flex justify-center items-center mt-[10rem]">
+  //   <div className="Man h-screen flex justify-center items-center">
      
   //   {/* forms */}
-  //   <div className=" border-red-400 bg-red-200 border-2 w-[22rem] justify-center items-center h-[22rem]">
+  //   <div className=" border-gray-50 backdrop-blur-lg border-2 w-[22rem] justify-center items-center h-[28rem]">
   //   <form action="">
 
   //    <div className=" p-3">
-  //    <input type="text"placeholder="Username" onChange={(e)=> setName(e.target.value)} className=" pl-3  h-[3rem] w-[20rem]"  /> <br /> <br />
-  //    <input type="text"placeholder="Password" onChange={(e)=> setName(e.target.value)} className=" pl-3  h-[3rem] w-[20rem]"  />
+  //    <input type="text"placeholder="" onChange={(e)=> setName(e.target.value)} className=" text-white text-xl pl-3 bg-transparent 
+  //    border-2 border-gray-50/80 outline-none  h-[3rem] w-[20rem]"  /> <br /> <br />
+  //    <input type="text"placeholder="" onChange={(e)=> setName(e.target.value)} className=" text-white text-xl pl-3 bg-transparent 
+  //    border-2 border-gray-50/80 outline-none  h-[3rem] w-[20rem]"  />
   //    </div>
   //    <br />
     
-
+  //    <p className=" text-sm ml-4 text-black">Display screen</p>
   //    <div className=" bg-gray-600 text-white pl-3 m-4 h-12 items-center text-3xl flex">
   //    <h1>{Name}</h1>
   //    </div>
 
-  // <div className=" flex justify-center items-center mt-[3rem]">
-  // <button className=" bg-blue-300 w-[5rem] py-2 hover:bg-blue-600/80 hover:text-gray-50 ">submit</button>
+  // <div className=" flex justify-center items-center mt-[8rem]">
+  // <button className=" bg-white w-[5rem] py-2 hover:bg-blue-600/80 hover:text-gray-50 ">submit</button>
   // </div>
   //   </form>
 
@@ -32,7 +34,7 @@ const Counter = () => {
     
 
   //   </div>
-  <div></div>
+ <div></div>
   );
 };
 
--- src/components/Login/LoginPage.jsx
+++ src/components/Login/LoginPage.jsx
@@ -37,6 +37,7 @@ function LoginPage() {
         </form>
       </div>
     </div>
+   
   );
 }
 
